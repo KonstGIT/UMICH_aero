2baaaa08fc8000a51a406fc5143353f6c594aafd
Konstantin Niehaus <kniehaus@umich.edu>
2017-09-14 15:54:19 -0400

    added quasi static solution and qep solver

--- divergence.py
+++ divergence.py
@@ -17,6 +17,7 @@ import matplotlib.ticker as mtick
 from matplotlib.backends.backend_pdf import PdfPages
 from numpy import linalg as LA
 from numpy.linalg import inv
+from numpy.linalg import det
 
 
 
@@ -40,7 +41,7 @@ def Re(kin_visc,c,U):
 def cla(U,kin_visc,c,plot=True,profile="0012",pdfobj=-1): #1/rad 
     filename='naca'+profile+"_"+str(int(U))
     
-    calcPolar(profile, int(Re(kin_visc,c,U)), filename, alfaseq=np.linspace(0,8,50))
+    calcPolar(profile, int(Re(kin_visc,c,U)), filename, alfaseq=np.linspace(0,4,10))
     inp = np.loadtxt(filename,skiprows=12)
     inp = np.append(inp,-inp[1:,:],axis=0)
     AOA=inp[:,0]
@@ -61,13 +62,56 @@ def cla(U,kin_visc,c,plot=True,profile="0012",pdfobj=-1): #1/rad
            pdfobj.savefig()   
            
     return(p[0])
+    
+def claclm(U,kin_visc,c,plot_cl=False,plot_cm=False,profile="0012",pdfobj=-1): #1/rad 
+    filename='naca'+profile+"_"+str(int(U))   
+    calcPolar(profile, int(Re(kin_visc,c,U)), filename, alfaseq=np.linspace(0,4,10))
+    inp = np.loadtxt(filename,skiprows=12)
+    inp = np.append(inp[:,:],-inp[:,:],axis=0)
+
+    AOA=inp[:,0]
+    CL=inp[:,1]
+    CM=inp[:,4]
+
+    p1=polyfit(AOA*np.pi/180.,CL,1)
+    p2=polyfit(AOA*np.pi/180.,CM,1)
+    if plot_cl:
+        fig, ax = plt.subplots()
+        plt.plot(AOA*np.pi/180.,CL,".",label=r"$\mathrm{Num. data}$",color=[.2, .2, .2])  
+        plt.plot(AOA*np.pi/180.,p1[0]*AOA*np.pi/180.,label=r"$\mathrm{Linear regression}$",color="red")
+        plt.title(r"$\mathrm{NACA"+profile+"}\ U_\infty="+str('%0.2f' %U)+"\ ms^{-1}\ \mathrm{Re}="+str(int(Re(kin_visc,c,U)))+"$")
+        plt.xlabel(r"$\mathrm{Angle\ of\ attack}\ [rad]$")
+        plt.ylabel(r"$c_l\ [-]$")
+        ax.spines['right'].set_visible(False)
+        ax.spines['top'].set_visible(False)
+        ax.xaxis.set_ticks_position('bottom')
+        ax.yaxis.set_ticks_position('left')  
+        if pdfobj!=-1:
+           pdfobj.savefig()   
+    if plot_cm:
+        fig, ax = plt.subplots()
+        print(CM)
+        print(AOA)
+        plt.plot(AOA*np.pi/180.,CM,".",label=r"$\mathrm{Num. data}$",color=[.2, .2, .2])  
+        plt.plot(AOA*np.pi/180.,p2[0]*AOA*np.pi/180.,label=r"$\mathrm{Linear regression}$",color="red")
+        plt.title(r"$\mathrm{NACA"+profile+"}\ U_\infty="+str('%0.2f' %U)+"\ ms^{-1}\ \mathrm{Re}="+str(int(Re(kin_visc,c,U)))+"$")
+        plt.xlabel(r"$\mathrm{Angle\ of\ attack}\ [rad]$")
+        plt.ylabel(r"$c_m\ [-]$")
+        ax.spines['right'].set_visible(False)
+        ax.spines['top'].set_visible(False)
+        ax.xaxis.set_ticks_position('bottom')
+        ax.yaxis.set_ticks_position('left')  
+        if pdfobj!=-1:
+           pdfobj.savefig() 
+           
+    return(p1[0],p2[0])
 
 def Ka(k,x_ea,x_sp):
     return k*np.abs(x_ea-x_sp)**2
     
 def eff_stiff(U,rho,l,c,x_ea,x_ac,x_sp,k,kin_visc,profile="0012",pdfobj=-1): # effective stiffness
     S=l*c
-    return 4*Ka(k,x_ea,x_sp)-S*cla(U,kin_visc,c,profile=profile,pdfobj=pdfobj)*.5*U*U*rho*(x_ea-x_ac)
+    return 8*Ka(k,x_ea,x_sp)-S*cla(U,kin_visc,c,profile=profile,pdfobj=pdfobj)*.5*U*U*rho*(x_ea-x_ac)
 
 def get_div_speed(Umax,l,c,x_ea,x_sp,x_ac,k,plot=False,steps=20,Umin=.5,rho=1.2,kin_visc=15.11E-6,profile="0012",pdfobj=-1):
     U=np.linspace(Umin,Umax,steps)
@@ -86,7 +130,7 @@ def get_div_speed(Umax,l,c,x_ea,x_sp,x_ac,k,plot=False,steps=20,Umin=.5,rho=1.2,
         ax.xaxis.set_ticks_position('bottom')
         ax.yaxis.set_ticks_position('left')  
         plt.xlim([0,Umax*1.1])  
-        fmt = r'$%1.0f$'
+        fmt = r'$%1.1f$'
         xticks = mtick.FormatStrFormatter(fmt)
         ax.xaxis.set_major_formatter(xticks)            
         if pdfobj!=-1:
@@ -101,12 +145,12 @@ def get_Ud():
     x_ea=0.5*c #m
     x_ac=0.25*c #m
     k=14.53 #N/m    
-    Umax=10. # m/s
+    Umax=5. # m/s
     pp = PdfPages("naca0012_divergencespeed")
-    get_div_speed(Umax,l,c,x_ea,x_sp,x_ac,k,steps=20,plot=True,profile="0012",pdfobj=pp)
+    get_div_speed(5,l,c,x_ea,x_sp,x_ac,k,steps=20,plot=True,profile="0012",pdfobj=pp)
     pp.close()
     
-def get_flutter():
+def get_flutter(plot=False):
     
     c=0.108
     x_ea=0.35*c # elastic axis [m]
@@ -128,28 +172,68 @@ def get_flutter():
     e=x_ea-x_ac #m
     
     Umin=0.5
-    Umax=10
+    Umax=50
     
-    U=np.linspace(Umin,Umax,10)
-    EV=np.zeros([len(U),2])
+    U=np.linspace(Umin,Umax,1)
+    EVs=np.zeros([len(U),3])+0j
+    EVqs=np.zeros([len(U),5])+0j
     for i in range(len(U)):
-        EV[i,:]=flutter_static(U[i],kh,ka,S,m,Sa,Ia,e,c)
-    return EV
-    
+#        EVs[i,:]=np.append(U[i],flutter_st(U[i],kh,ka,S,m,Sa,Ia,e,c))
+        EVqs[i,:]=np.append(U[i],flutter_qst(U[i],kh,ka,S,m,Sa,Ia,e,c))
+        
+        
+    if plot:
+        fig, ax = plt.subplots()
+        plt.plot(EVqs[:,0],np.imag(EVqs[:,1]),".",color=[.05,.05,.05],label=r"$\mathrm{Eigenvalue}\ 1$")
+        plt.plot(EVqs[:,0],np.imag(-EVqs[:,1]),".",color=[.05,.05,.05])
+        plt.plot(EVqs[:,0],np.imag(EVqs[:,2]),".",color=[.1,.5,.1],label=r"$\mathrm{Eigenvalue}\ 2$")
+        plt.plot(EVqs[:,0],-np.imag(EVqs[:,2]),".",color=[.1,.5,.1])
+        plt.legend(loc=3)
+        plt.xlabel(r'$\mathrm{Flow\ velocity\ [m/s]}$')
+        plt.ylabel(r'$\mathrm{Im}\ EV$')
+        plt.title(r"$\mathrm{Imag.\ part\ of\ eigenvalues\ static}$") 
+        fmt = r'$%1.0f$'
+        xticks = mtick.FormatStrFormatter(fmt)
+        ax.xaxis.set_major_formatter(xticks) 
+        yticks = mtick.FormatStrFormatter(fmt)
+        ax.yaxis.set_major_formatter(yticks)     
+        ax.spines['right'].set_visible(False)
+        ax.spines['top'].set_visible(False)
+        ax.xaxis.set_ticks_position('bottom')
+        ax.yaxis.set_ticks_position('left')      
 
 # static
-def flutter_static(U,Kh,Ka,S,m,Sa,Ia,e,c,kin_visc=15.11E-6,profile="0012",rho=1.22):
+def flutter_st(U,Kh,Ka,S,m,Sa,Ia,e,c,kin_visc=15.11E-6,profile="0012",rho=1.22):
     # generate matrix
-    dclda=cla(U,kin_visc,c)
+    dclda,dcmda=cladcm(U,kin_visc,c,plot=False)
     q=.5*rho*U**2
     A=np.array([[Kh, q*S*dclda],[0, Ka-e*q*S*dclda]])
     B=inv(np.array([[m, Sa],[Sa, Ia]]))
-    ev=np.asarray(np.sqrt(LA.eig(np.dot(B,A))[0]+0j))
+    ev=np.asarray(np.sqrt(LA.eig(np.dot(B,A))[0]+0j))    
     return ev
     
 # quasi-static
-def flutter_quasistatic():
-    return 0  
+def flutter_qst(U,Kh,Ka,S,m,Sa,Ia,e,c,kin_visc=15.11E-6,profile="0012",rho=1.22):
+    dclda,dcmda=claclm(U,kin_visc,c,plot_cm=False,plot_cl=False)
+    q=.5*rho*U**2
+    
+    M=np.array([[m, Sa],[Sa, Ia]])
+    D=np.array([[q*S*dclda/U,0],[e*q*S*dclda/U,-q*S*c*dcmda]])
+    K=np.array([[Kh, q*S*dclda],[0, Ka-e*q*S*dclda]])
+    
+    A=np.zeros([4,4])
+    A[:2,:2]=M
+    A[2:,2:]=np.diag(np.ones(2))
+    
+    B=np.zeros([4,4])
+    B[:2,:2]=D
+    B[:2,2:]=K
+    B[2:,:2]=-np.diag(np.ones(2))
+    
+    Ai=inv(A)
+    ev=LA.eig(np.dot(Ai,B)+0j)  
+    print(ev)
+    return ev[0]
     
 # theodorsen garrik    
 def flutter_theodorsen_garrik():
@@ -157,8 +241,8 @@ def flutter_theodorsen_garrik():
 
 
 
-get_Ud()
-#get_flutter
+#get_Ud()
+get_flutter(plot=True)
 
 #generate_lookup_clalph(Umin,Umax,steps,l,c,x_ea,x_ac,x_sp,k,filename,rho=1.2,kin_visc=15.11E-6)
 #get_Ud()
