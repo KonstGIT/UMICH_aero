c79ea204d86931e4959d25d4c80f363627fecedf
Konstantin Niehaus <kniehaus@umich.edu>
2017-09-13 21:57:28 -0400

    removed old flutter functions finished static flutter function

--- divergence.py
+++ divergence.py
@@ -18,29 +18,7 @@ from matplotlib.backends.backend_pdf import PdfPages
 from numpy import linalg as LA
 from numpy.linalg import inv
 
-def generate_lookup_clalph(Umin,Umax,steps,l,c,x_ea,x_ac,x_sp,k,filename,rho=1.2,kin_visc=15.11E-6):
-    U=np.linspace(Umin,Umax,steps)    
-    crit=np.zeros(len(U))
-    for i in range(len(U)):
-        crit[i]=div_crit(U[i],rho,l,c,x_ea,x_ac,x_sp,k,kin_visc)
-    np.save(filename, [U,crit]) 
 
-def get_lookup_clalph(filename,plot=False):
-    lookup=np.load(filename)
-    U=lookup[0,:]
-    cla=lookup[1,:]
-    if plot:
-        fig, ax = plt.subplots()
-        u=np.linspace(Umin,Umax,steps)
-        plt.plot(u,f(u),'or')
-        plt.xlabel(r'$\mathrm{Flow\ velocity\ [m/s]}$')
-        plt.ylabel(r'$\mathrm{c_{l,\alpha}}$')
-        plt.title(r"$\mathrm{Divergence\ velocity\ from\ effective\ stiffness}$") 
-        ax.spines['right'].set_visible(False)
-        ax.spines['top'].set_visible(False)
-        ax.xaxis.set_ticks_position('bottom')
-        ax.yaxis.set_ticks_position('left')         
-    return interpolate.interp1d(U, cla)
 
 def flutter_speed(filename,U,m,Ia,Sa,k,x_ea,x_sp,x_cg,x_ac,c,l,rho=1.2):   
     cla=get_lookup_clalph(filename)
@@ -54,30 +32,12 @@ def flutter_speed(filename,U,m,Ia,Sa,k,x_ea,x_sp,x_cg,x_ac,c,l,rho=1.2):
     B=kh*Ia+(ka-e*q*S*cla(U))*m-q*S*cla(U)*Sa+0j
     C=kh*(ka-e*q*S*cla(U))+0j
     p2_1=(-B+np.sqrt(B**2-4*A*C))/(2*A)    
-    p2_2=(-B-np.sqrt(B**2-4*A*C))/(2*A) 
-    
-# static
-def flutter_static(U,cla,rho,Kh,Ka,S,m,Sa,Ia,e):
-    # generate matrix
-    q=.5*rho*U**2
-    A=np.array([[Kh, q*S*cla],[0, Ka-e*q*S*cla]])
-    B=inv(np.array([[m, Sa],[Sa, Ia]]))
-    print(np.sqrt(LA.eig(np.dot(B,A))))   
-    return 0
-    
-# quasi-static
-def flutter_quasistatic():
-    return 0  
-    
-# theodorsen garrik    
-def flutter_theodorsen_garrik():
-    return 0     
-    
+    p2_2=(-B-np.sqrt(B**2-4*A*C))/(2*A)   
     
 def Re(kin_visc,c,U):
     return U*c/kin_visc
 
-def cla(U,kin_visc,c,plot=True,profile="0012",pdfobj=-1): #1/rad
+def cla(U,kin_visc,c,plot=True,profile="0012",pdfobj=-1): #1/rad 
     filename='naca'+profile+"_"+str(int(U))
     
     calcPolar(profile, int(Re(kin_visc,c,U)), filename, alfaseq=np.linspace(0,8,50))
@@ -103,11 +63,11 @@ def cla(U,kin_visc,c,plot=True,profile="0012",pdfobj=-1): #1/rad
     return(p[0])
 
 def Ka(k,x_ea,x_sp):
-    return 4.*k*np.abs(x_ea-x_sp)
+    return k*np.abs(x_ea-x_sp)**2
     
 def eff_stiff(U,rho,l,c,x_ea,x_ac,x_sp,k,kin_visc,profile="0012",pdfobj=-1): # effective stiffness
     S=l*c
-    return Ka(k,x_ea,x_sp)-S*cla(U,kin_visc,c,profile=profile,pdfobj=pdfobj)*.5*U*U*rho*(x_ea-x_ac)
+    return 4*Ka(k,x_ea,x_sp)-S*cla(U,kin_visc,c,profile=profile,pdfobj=pdfobj)*.5*U*U*rho*(x_ea-x_ac)
 
 def get_div_speed(Umax,l,c,x_ea,x_sp,x_ac,k,plot=False,steps=20,Umin=.5,rho=1.2,kin_visc=15.11E-6,profile="0012",pdfobj=-1):
     U=np.linspace(Umin,Umax,steps)
@@ -146,30 +106,59 @@ def get_Ud():
     get_div_speed(Umax,l,c,x_ea,x_sp,x_ac,k,steps=20,plot=True,profile="0012",pdfobj=pp)
     pp.close()
     
-def test_get_flutter():
-
-    Umin=2
-    Umax=15
-    steps=500
+def get_flutter():
     
-    c=0.108 #m
-    l=0.295 #m
-    spring_pos=np.array([0.2,0.5])*c
-    x_sp=0.5*c #m
-    x_ea=np.mean(spring_pos) #m
-    x_ac=0.25*c #m
-    x_cg=0.4545*c #m
+    c=0.108
+    x_ea=0.35*c # elastic axis [m]
+    x_cg=0.4545*c # center of gravity [m]
+    x_ac=0.25*c # aerodynamic center [m]
+    x_sp=0.5*c # spring location
+    
+    k=14.53 #N/m
+    kh=8*k #N/m
+    ka=8*Ka(k,x_ea,x_sp) #Nm/rad
+    
     m=0.071 #kg
-    Ia=3.92E-4 #kg/m/l
+    Ia=3.92E-4 #kg m
+    
+    l=0.295
+    S=c*l #m**2
+    
     Sa=(x_ea-x_cg)*m #kg m
-    k=2*14.53#N/m  
-    lookup_file="cl_alpha_lookup.npy"
-    U=9.
+    e=x_ea-x_ac #m
+    
+    Umin=0.5
+    Umax=10
+    
+    U=np.linspace(Umin,Umax,10)
+    EV=np.zeros([len(U),2])
+    for i in range(len(U)):
+        EV[i,:]=flutter_static(U[i],kh,ka,S,m,Sa,Ia,e,c)
+    return EV
+    
 
-    flutter_speed(lookup_file,U,m,Ia,Sa,k,x_ea,x_sp,x_cg,x_ac,c,l)
+# static
+def flutter_static(U,Kh,Ka,S,m,Sa,Ia,e,c,kin_visc=15.11E-6,profile="0012",rho=1.22):
+    # generate matrix
+    dclda=cla(U,kin_visc,c)
+    q=.5*rho*U**2
+    A=np.array([[Kh, q*S*dclda],[0, Ka-e*q*S*dclda]])
+    B=inv(np.array([[m, Sa],[Sa, Ia]]))
+    ev=np.asarray(np.sqrt(LA.eig(np.dot(B,A))[0]+0j))
+    return ev
+    
+# quasi-static
+def flutter_quasistatic():
+    return 0  
+    
+# theodorsen garrik    
+def flutter_theodorsen_garrik():
+    return 0   
 
-flutter_static(1,2,3,4,5,6,7,8,9,10)
-#test_get_Ud()
+
+
+get_Ud()
+#get_flutter
 
 #generate_lookup_clalph(Umin,Umax,steps,l,c,x_ea,x_ac,x_sp,k,filename,rho=1.2,kin_visc=15.11E-6)
 #get_Ud()
@@ -178,31 +167,4 @@ flutter_static(1,2,3,4,5,6,7,8,9,10)
 
 
 
-#fmt = r'$%1.1f$'
-#xticks = mtick.FormatStrFormatter(fmt)
-#ax.xaxis.set_major_formatter(xticks)
-#fmt = r'$%1.0f$'
-#yticks = mtick.FormatStrFormatter(fmt)
-#ax.yaxis.set_major_formatter(yticks)
 
-
-
-# make sure clalpha is the slope not cl
-# make clalpha a function of u
-
-
-#case="Remax"
-#print(case)
-#print(Ufc(case,rho,l,c,x_ea,x_sp,k))
-#case="Remid"
-#print(case)
-#print(Ufc(case,rho,l,c,x_ea,x_sp,k))
-#case="Remin"
-#print(case)
-#print(Ufc(case,rho,l,c,x_ea,x_sp,k))
-#case="_4"
-#print(case)
-#print(Ufc(case,rho,l,c,x_ea,x_sp,k))
-#case="_6"
-#print(case)
-#print(Ufc(case,rho,l,c,x_ea,x_sp,k))
